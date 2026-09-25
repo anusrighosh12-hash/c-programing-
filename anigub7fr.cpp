a36a5886889a83397a124643f79bf3cacd6c2d7b
anusrighosh12-hash <anusrighosh12@gmail.com>
2026-09-25 17:21:21 +0530

    Add files via upload

---
+++ anigub7fr.cpp
@@ -0,0 +1,17 @@
+// write a c program is play odd numbers 1 to n
+#include <stdio.h>
+int main ()
+{
+    int n;
+    int i=1;
+printf("Enter the number:n");
+scanf("%d",&n);
+while (i<=n)
+{
+	printf("\n %d",i);
+	i++;
+}
+}
+
+	
+
